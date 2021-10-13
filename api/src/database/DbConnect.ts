68402ef67ea000c105c90e0c8d572c01f724c8fb
Benedict <jokanolaadedipupo@gmail.com>
2021-10-13 03:57:02 +0100

    connecting to database

--- api/src/database/DbConnect.ts
+++ api/src/database/DbConnect.ts
@@ -10,11 +10,26 @@ const logger = winston.createLogger({
 
 const connectDB = () => {
   const url: string =  process.env.DATABASE_URL as string;
-  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-    .then(() => {
-      logger.log('info', 'Successfully connected to MongoDB Atlas!');
-    }).catch((error) => {
-      logger.log('error', error.message);
-    });
+  mongoose.connect(url!, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    server: {
+        poolSize: Number(process.env.POOL_SIZE!)
+    }
+})
+.then((res) => {
+    console.log(
+        'Connected to Distribution API Database - Initial Connection'
+    );
+})
+.catch((err) => {
+    console.log(
+        `Initial Distribution API Database connection error occured -`,
+        err
+    );
+});
 };
 export default connectDB;
+
