0e3434bb5d34e7049b5782ba87fe5d68f453e0b7
Benedict <jokanolaadedipupo@gmail.com>
2021-10-13 13:41:12 +0100

    login user

--- api/src/controllers/user.ts
+++ api/src/controllers/user.ts
@@ -56,4 +56,4 @@ export const loginUser =  asyncHandler(async function(
         },
       })
     }
-}
+});
--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -1,12 +1,14 @@
 import { model, Schema } from 'mongoose';
 import { IUser } from '../types/types';
+import bcrypt from "bcryptjs";
+
 
 const schema = new Schema<IUser>({
   username: { type: String, required: true },
   password: { type: String, required: true },
 });
 
-userSchema.methods.matchPassword = async function (enteredPassword) {
+schema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword,this.password)
 }
 
