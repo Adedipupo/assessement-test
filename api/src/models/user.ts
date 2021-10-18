a36d04a50c5b05c9db61f410bb9a6459ef008983
Benedict <jokanolaadedipupo@gmail.com>
2021-10-18 18:04:17 +0100

    authentication

--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcryptjs";
 
 const UserSchema = new Schema<IUser>({
   username: { type: String, required: true },
+  email: { type: String, required: true },
   password: { type: String, required: true },
 });
 
